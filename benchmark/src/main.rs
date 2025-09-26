a60a37da9f58b31c1640e6d2a8b6ce1c738edf3b
FoxMoss <foxmoss@mediaology.com>
2025-09-25 19:37:29 -0500

    bench against one milli delay

--- benchmark/src/main.rs
+++ benchmark/src/main.rs
@@ -1,4 +1,4 @@
-use std::{os::unix::process::CommandExt, process::Command, thread};
+use std::{ops::Deref, os::unix::process::CommandExt, process::Command, thread};
 
 mod client;
 mod server;
@@ -6,14 +6,14 @@ use charming::{ImageFormat, ImageRenderer};
 use std::sync::mpsc;
 use tokio::runtime::Runtime;
 
-use crate::server::chart_stats;
+use crate::server::{Filter, chart_stats};
 
-fn test(duration: u64) -> server::Stats {
+fn test(duration: u64, filter: Option<Filter>) -> client::Stats {
     let (tx, rx) = mpsc::channel::<server::Messages>();
     let server_thread = thread::spawn(move || {
         let rt = Runtime::new().expect("Failed to create Tokio runtime");
         rt.block_on(async move {
-            server::run_server(tx, duration)
+            server::run_server(tx, duration, filter)
                 .await
                 .expect("Server failed")
         })
@@ -21,14 +21,14 @@ fn test(duration: u64) -> server::Stats {
 
     let client_thread = thread::spawn(|| {
         let rt = Runtime::new().expect("Failed to create Tokio runtime");
-        rt.block_on(async move {
-            client::run_client(rx).await.expect("Client failed");
-        })
+        rt.block_on(async move { client::run_client(rx).await.expect("Client failed") })
     });
 
-    let stats = server_thread.join().unwrap();
-    client_thread.join().unwrap_or_default();
-    return stats;
+    server_thread.join().unwrap();
+    let stats = client_thread.join().unwrap();
+    let stats = stats.lock().unwrap();
+
+    return stats.deref().clone();
 }
 
 fn main() {
@@ -47,14 +47,18 @@ fn main() {
         "../build/fox.bpf",
     ]);
     let mut child_filter = fox_filter_command.spawn().expect("failed to spawn");
-    let xdp = test(duration);
+    let xdp = test(duration, None);
     child_filter.kill().expect("failed to kill");
 
-    let no_filter = test(duration);
+    let no_filter = test(duration, None);
+
+    let slowdown_filter = test(duration, Some(Filter::OneMillisecondsSlowDown));
+
 
     let chart = chart_stats(
         vec![
             (no_filter, "No filter".to_string()),
+            (slowdown_filter, "Delay by 1ms".to_string()),
             (xdp, "Calculating Jenkins hash in XDP".to_string()),
         ],
         buckets,
